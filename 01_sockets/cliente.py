6c185621dd906e249ac03ae4baedde0ad531b463
Miguel A Fernandez <migue2801224@gmail.com>
2026-02-09 23:05:02 -0500

    programa_cliente_sockets

--- 01_sockets/cliente.py
+++ 01_sockets/cliente.py
@@ -1,1 +1,12 @@
-# Cliente
+import socket
+HOST = 'localhost'
+PORT = 9000
+
+cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+cliente.connect((HOST, PORT))
+
+cliente.sendall(b"Mundo!")
+respuesta = cliente.recv(1024)
+print(f"Repuesta: {respuesta}")
+
+cliente.close()
