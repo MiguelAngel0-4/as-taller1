e8d13844a41686f20f33f5c96bf4c4810b3d6981
Miguel A Fernandez <migue2801224@gmail.com>
2026-02-09 22:53:34 -0500

    programa_servidor_sockets

--- 01_sockets/servidor.py
+++ 01_sockets/servidor.py
@@ -1,3 +1,15 @@
-# Servidor
-#prueba
-#2 prueba
+import socket
+HOST = 'localhost'
+PORT = 9000
+
+servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+servidor.bind((HOST, PORT))
+servidor.listen()
+print("el servidor esta a la espera de conexiones ...")
+
+cliente, direccion = servidor.accept()
+print(f"un cliente se conectó desde la dirección {direccion}")
+
+datos = cliente.recv(1024)
+cliente.sendall(b"HOla! " + datos) #ojo! debe ser binario, no cadena
+cliente.close()
