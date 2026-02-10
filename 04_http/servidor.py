daeb666dce83b6ee29890c415252757eb28b39bd
Miguel A Fernandez <migue2801224@gmail.com>
2026-02-10 00:42:25 -0500

    programa_servidor_HTTP

--- 04_http/servidor.py
+++ 04_http/servidor.py
@@ -1,2 +1,12 @@
-# Servidor
+import socket
+import http.server
+
+HOST = 'localhost'
+PORT = 9000
+
+class Servidor(http.server.SimpleHTTPRequestHandlermple):
+    pass
+
+Servidor = http.server.HTTPServer((HOST, PORT), Servidor)
+Servidor.serve_forever()
 
